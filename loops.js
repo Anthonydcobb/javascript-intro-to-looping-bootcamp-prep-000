6720ced68228f1fca5396cad6e9e6797e0fc7f62
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-15 18:28:16 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,27 @@
+var forLoop = (array) => {
+  for (let i = 0; i <= 24; i++) {
+    i === 1 ? array.push(`I am ${i} strange loop.`) : array.push(`I am ${i} strange loops.`)
+  }
+  return array;
+}
+
+var whileLoop = (n) => {
+  while (n >= 1) {
+    console.log(n);
+    n--;
+    }
+    console.log("done")
+  }
+
+  var incrementVariable = () => {
+    i = i + 1;
+    return i;
+  }
+
+  var i = 1;
+
+var doWhileLoop = (num) => {
+  do {
+    "I run once regardless"
+  } while (incrementVariable() < num)
+}
